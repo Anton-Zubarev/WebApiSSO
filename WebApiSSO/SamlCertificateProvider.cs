32c63ca8217b28905e37bcbfb6c139395952a037
Anton-Zubarev <anton.zubarev@gmail.com>
2026-02-02 11:20:33 +0300

    First commit

---
+++ WebApiSSO/SamlCertificateProvider.cs
@@ -0,0 +1,45 @@
+﻿namespace WebApiSSO;
+
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+public class SamlCertificateProvider
+{
+    public X509Certificate2 Certificate { get; }
+    public string ValidIssuer { get; }
+    /// <summary>
+    /// В корне должен быть FederationMetadata.xml
+    /// (качать из https://sso.carcade.com/FederationMetadata/2007-06/FederationMetadata.xml)
+    /// </summary>
+    /// <exception cref="Exception"></exception>
+    public SamlCertificateProvider()
+    {
+        string baseDir = AppContext.BaseDirectory;
+        string xmlPath = Path.Combine(baseDir, "FederationMetadata.xml");
+
+        XDocument xmlDoc = XDocument.Load(xmlPath);
+
+        XNamespace md = "urn:oasis:names:tc:SAML:2.0:metadata";
+        XNamespace ds = "http://www.w3.org/2000/09/xmldsig#";
+
+        var certificateBase64 = xmlDoc.Root?
+            .Element(ds + "Signature")?
+            .Element(ds + "KeyInfo")?
+            .Element(ds + "X509Data")?
+            .Element(ds + "X509Certificate")?.Value ?? throw new ArgumentNullException("X509Certificate");
+
+        var validIssuer = xmlDoc.Root?.Attribute("entityID")?.Value;
+
+        byte[] certBuffer = Convert.FromBase64String(certificateBase64);
+        var certificate = new X509Certificate2(certBuffer);
+
+        Certificate = certificate;
+        ValidIssuer = validIssuer ?? throw new ArgumentNullException(nameof(validIssuer));
+    }
+
+    public SamlCertificateProvider(X509Certificate2 certificate, string validIssuer)
+    {
+        Certificate = certificate ?? throw new ArgumentNullException(nameof(certificate));
+        ValidIssuer = validIssuer ?? throw new ArgumentNullException(nameof(validIssuer));
+    }
+}
---
+++ WebApiSSO/readme.md
@@ -0,0 +1,27 @@
+﻿# Авторизация WS Federations (ADFS)
+
+Подключение авторизации ADFS к существующему проекту.
+Прежде он использовал Windows авторизацию.
+
+Простое повторение кода из https://learn.microsoft.com/ru-ru/aspnet/core/security/authentication/ws-federation?view=aspnetcore-10.0
+
+```` java
+services.AddAuthentication()
+.AddWsFederation(options =>
+{
+    // MetadataAddress represents the Active Directory instance used to authenticate users.
+    options.MetadataAddress = "https://<ADFS FQDN or AAD tenant>/FederationMetadata/2007-06/FederationMetadata.xml";
+
+    // Wtrealm is the app's identifier in the Active Directory instance.
+    // For ADFS, use the relying party's identifier, its WS-Federation Passive protocol URL:
+    options.Wtrealm = "https://localhost:44307/";
+});
+````
+
+не сработало.
+
+Как я понял причину - сертификат скачивается или долго или с ошибкой. Но точного понимания найти не смог.
+
+Тогда я сделал файл сертификата статическим файлом и в коде зполняю из него нужные поля.
+
+
